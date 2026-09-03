97cf3b1f028f84fac2c4f13b0b4754e63474be75
Syed Ahmad ALi Rizvi <108014491+ahmadalirizvi@users.noreply.github.com>
2026-09-03 23:02:41 +0500

    phase 1

--- main.py
+++ main.py
@@ -2,34 +2,37 @@
 
 data = {}
 
-data["name"] = "Ahmad"
-print(data["name"])
+# data["name"] = "Ahmad"
+# print(data["name"])
 
 class key_value_store:
     def __init__(self):
         self.data = {}
         
         
-    def set(key, value):
-        data[key] = value
+    def set(self, key, value):
+        self.data[key] = value
         return "OK"
     
-    def get_value(key):
-        if key in data:
-            return data[key]
+    def get_value(self, key):
+        if key in self.data:
+            return self.data[key]
         return None
     
 
-    def del_value(key):
-        if key in data:
-            del data[key]
+    def del_value(self, key):
+        if key in self.data:
+            del self.data[key]
             return "OK"
         return "Key not found"
     
-    def exists(key):
-        return key in data
+    def exists(self, key):
+        return key in self.data
     
 
-set("name", "Ahmad")
-set("age", 21)
+store = key_value_store()
+store.set("name", "Ahmad")
+store.set("age", 21)
+print(store.get_value("name"))
+
 
