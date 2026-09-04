efe775e50fed40e6c5562139431c7c14162d36b4
Syed Ahmad ALi Rizvi <108014491+ahmadalirizvi@users.noreply.github.com>
2026-09-04 17:05:17 +0500

    Update client.py

--- client.py
+++ client.py
@@ -6,9 +6,18 @@ PORT = 6380
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        s.sendall(b"SET name Ahmad\n")
-        response = s.recv(1024)
-        print("Server said:", response.decode())
+
+        commands = [
+            "SET name Ahmad",
+            "GET name",
+            "DELETE name",
+        ]
+
+        for cmd in commands:
+            s.sendall((cmd + "\n").encode())
+            response = s.recv(1024)
+            print(f"> {cmd}")
+            print("Server said:", response.decode())
 
 if __name__ == "__main__":
     main()
